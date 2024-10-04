4f05999788a474f95a48c867f90c203facfc6c23
Rajendra Moharana <raju.devops24@outlook.com>
2024-10-04 09:21:44 +0530

    Added

--- Day8/backend.tf
+++ Day8/backend.tf
@@ -2,7 +2,7 @@ terraform   {
     backend "azurerm" {
         resource_group_name = "raju-rg"
         storage_account_name = "rajustg"
-        container_name = "prod.tfstate"
+        container_name = "prod-tfstate"
         key = "prod.terraform.tfstate"
     }
 }
